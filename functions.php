7cc76aa368f41cf75fd5b81eb933955dab0fa8d3
KiwiSolutions <54039753+KiwiSolutions@users.noreply.github.com>
2020-05-28 21:09:20 +0200

    deleted and added new child theme files

--- functions.php
+++ functions.php
@@ -1,23 +1,20 @@
-<?php
-/**
- * Divi Cake Child Theme
- * Functions.php
- *
- * ===== NOTES ==================================================================
- * 
- * Unlike style.css, the functions.php of a child theme does not override its 
- * counterpart from the parent. Instead, it is loaded in addition to the parent's 
- * functions.php. (Specifically, it is loaded right before the parent's file.)
- * 
- * In that way, the functions.php of a child theme provides a smart, trouble-free 
- * method of modifying the functionality of a parent theme. 
- * 
- * Discover Divi Child Themes: https://divicake.com/products/category/divi-child-themes/
- * Sell Your Divi Child Themes: https://divicake.com/open/
- * 
- * =============================================================================== */
- 
-function divichild_enqueue_scripts() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
-add_action( 'wp_enqueue_scripts', 'divichild_enqueue_scripts' );
+<?php
+/**
+ * @author Divi Space
+ * @copyright 2017
+ */
+if (!defined('ABSPATH')) die();
+
+function ds_ct_enqueue_parent() { wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); }
+
+function ds_ct_loadjs() {
+	wp_enqueue_script( 'ds-theme-script', get_stylesheet_directory_uri() . '/ds-script.js',
+        array( 'jquery' )
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'ds_ct_enqueue_parent' );
+add_action( 'wp_enqueue_scripts', 'ds_ct_loadjs' );
+
+include('login-editor.php');
+?>
